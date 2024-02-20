5ec750863b28fef0622374cd80166011c59f22ff
alkuzin <alkuzindev@gmail.com>
2024-02-20 20:10:02 +0300

    feat: added mode function

--- include/statistics.hpp
+++ include/statistics.hpp
@@ -1,15 +1,18 @@
 #ifndef TORUS_STATISTICS_H
 #define TORUS_STATISTICS_H
 
-#include <vector>
-#include <numeric>
 #include <algorithm>
+#include <numeric>
+#include <vector>
+#include <map>
 
 namespace tlib
 {
 	double mean(const std::vector<double> &data);	
 
 	double median(const std::vector<double> &data);	
+
+	double mode(const std::vector<double> &data);	
 }
 
 #endif  // TORUS_STATISTICS_H
--- src/statistics.cpp
+++ src/statistics.cpp
@@ -6,7 +6,7 @@ namespace tlib
 	double mean(const std::vector<double> &data)
 	{
 		if (data.empty())
-        	throw std::invalid_argument("vector is empty");
+        	throw std::invalid_argument("data is empty");
 
 		return (std::accumulate(data.begin(), data.end(), 0.0) / data.size());
 	}
@@ -27,5 +27,31 @@ namespace tlib
 			median = data.at(size / 2);
 			
 		return median;
+	}
+	
+	double mode(const std::vector<double> &data)
+	{
+		if (data.empty())
+        	throw std::invalid_argument("data is empty");
+		
+		std::map<double, int> frequency_map;
+		double mode;
+    	int max_frequency;
+
+    	for (const auto& value : data) {
+        	frequency_map[value]++;
+		}
+    	
+		mode = data.at(0);
+		max_frequency = 0;
+    	
+		for (const auto& pair : frequency_map) {
+        	if (pair.second > max_frequency) {
+            	max_frequency = pair.second;
+            	mode = pair.first;
+        	}
+   		}
+
+    	return mode;
 	}
 }
